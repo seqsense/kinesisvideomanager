f7eff684ca3f5d0029bbdc8c3d1d1cf135df191e
Atsushi Watanabe <atsushi.w@ieee.org>
2019-12-26 16:37:53 +0900

    Send OPUS audio through kinesis

--- putmedia/main.go
+++ putmedia/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/at-wat/ebml-go"
 	kvm "github.com/seqsense/kinesis-test/kinesisvideomanager"
+	"github.com/seqsense/sq-gst-go/appsink"
+	"github.com/seqsense/sq-gst-go/gstlaunch"
 )
 
 const (
@@ -28,24 +30,40 @@ func main() {
 		log.Fatal(err)
 	}
 
+	l, err := gstlaunch.New(
+		"pulsesrc ! " +
+			"audio/x-raw,rate=48000,channels=1,format=S16LE !" +
+			"queue max-size-buffers=5 ! " +
+			"opusenc bitrate=32000 bitrate-type=vbr frame-size=40 ! " +
+			"appsink name=sink",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sink, err := l.GetElement("sink")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	ch := make(chan *kvm.BlockWithBaseTimecode, 10)
 	chTag := make(chan *kvm.Tag, 10)
 	start := time.Now()
-	go func() {
-		for i := 0; ; i++ {
-			t := uint64(time.Now().Sub(start)) / 1000000
 
-			data := &kvm.BlockWithBaseTimecode{
-				Timecode: t,
-				Block: ebml.Block{
-					1, 0, true, false, ebml.LacingNo, false,
-					[][]byte{{0x30, 0x31}},
-				},
-			}
-			log.Printf("write: %v", data)
-			ch <- data
-			time.Sleep(100 * time.Millisecond)
+	as := appsink.New(sink, func(b []byte, n int) {
+		t := uint64(time.Now().Sub(start)) / 1000000
+
+		data := &kvm.BlockWithBaseTimecode{
+			Timecode: t,
+			Block: ebml.Block{
+				1, 0, true, false, ebml.LacingNo, false,
+				[][]byte{b},
+			},
 		}
-	}()
+		// log.Printf("write: %v", data)
+		ch <- data
+	})
+	defer as.Close()
+	l.Start()
+
 	pro.PutMedia(ch, chTag)
 }
