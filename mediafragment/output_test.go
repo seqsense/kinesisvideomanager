9198ab08a75888619a763d3ecc82eeee8b215406
Atsushi Watanabe <atsushi.w@ieee.org>
2021-03-26 14:15:52 +0900

    Add fragment list sorter by producer timestamp (#158)

---
+++ mediafragment/output_test.go
@@ -0,0 +1,83 @@
+// Copyright 2021 SEQSENSE, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mediafragment
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	kvam "github.com/aws/aws-sdk-go/service/kinesisvideoarchivedmedia"
+)
+
+func TestListFragmentsOutput(t *testing.T) {
+	testData := func() *ListFragmentsOutput {
+		return &ListFragmentsOutput{
+			&kvam.ListFragmentsOutput{
+				Fragments: []*kvam.Fragment{
+					&kvam.Fragment{FragmentNumber: aws.String("1234568000"), ProducerTimestamp: aws.Time(time.Unix(100005, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234567000"), ProducerTimestamp: aws.Time(time.Unix(100000, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234569000"), ProducerTimestamp: aws.Time(time.Unix(100015, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234570000"), ProducerTimestamp: aws.Time(time.Unix(100010, 0))},
+				},
+			},
+		}
+	}
+	t.Run("SortByFragmentNumber", func(t *testing.T) {
+		expected := &ListFragmentsOutput{
+			&kvam.ListFragmentsOutput{
+				Fragments: []*kvam.Fragment{
+					&kvam.Fragment{FragmentNumber: aws.String("1234567000"), ProducerTimestamp: aws.Time(time.Unix(100000, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234568000"), ProducerTimestamp: aws.Time(time.Unix(100005, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234569000"), ProducerTimestamp: aws.Time(time.Unix(100015, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234570000"), ProducerTimestamp: aws.Time(time.Unix(100010, 0))},
+				},
+			},
+		}
+
+		data := testData()
+		data.SortByFragmentNumber()
+		if !reflect.DeepEqual(expected, data) {
+			t.Errorf("Expected sort result:\n%v\ngot:\n%v", expected, data)
+		}
+
+		t.Run("Sort_alias", func(t *testing.T) {
+			data := testData()
+			data.Sort()
+			if !reflect.DeepEqual(expected, data) {
+				t.Errorf("Expected sort result:\n%v\ngot:\n%v", expected, data)
+			}
+		})
+	})
+	t.Run("SortByProducerTimestamp", func(t *testing.T) {
+		expected := &ListFragmentsOutput{
+			&kvam.ListFragmentsOutput{
+				Fragments: []*kvam.Fragment{
+					&kvam.Fragment{FragmentNumber: aws.String("1234567000"), ProducerTimestamp: aws.Time(time.Unix(100000, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234568000"), ProducerTimestamp: aws.Time(time.Unix(100005, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234570000"), ProducerTimestamp: aws.Time(time.Unix(100010, 0))},
+					&kvam.Fragment{FragmentNumber: aws.String("1234569000"), ProducerTimestamp: aws.Time(time.Unix(100015, 0))},
+				},
+			},
+		}
+
+		data := testData()
+		data.SortByProducerTimestamp()
+		if !reflect.DeepEqual(expected, data) {
+			t.Errorf("Expected sort result:\n%v\ngot:\n%v", expected, data)
+		}
+	})
+}
